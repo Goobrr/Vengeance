8ec94c23937c25ef3408f98372beeb2719f7e1e3
Goobrr <ggoobbrr@gmail.com>
2022-07-14 20:16:59 +0700

    gaming

---
+++ src/vengeance/Vengeance.java
@@ -0,0 +1,16 @@
+package vengeance;
+
+import mindustry.mod.*;
+
+public class Vengeance extends Mod{
+
+    public Vengeance(){
+
+    }
+
+    @Override
+    public void loadContent(){
+
+    }
+
+}
